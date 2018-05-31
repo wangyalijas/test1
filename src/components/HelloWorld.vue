c1e6b7a7083b466bc43ccaad35ecb647780ec73e
wangyalijas <1114920411@qq.com>
2018-05-31 15:18:14 +0800

    test webhook jenkins7

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -88,7 +88,7 @@ export default {
   name: 'HelloWorld121121',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js Appeqweq轻武器'
+      msg: 'Welcome to Your Vue.js Appeqweq轻武器企鹅我'
     }
   }
 }
